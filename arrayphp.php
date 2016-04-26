7057558ee549206993f2fb82ac000c2ce0d2a893
geekgeek <solq87@gmail.com>
2016-04-27 01:37:57 +0200

    Create arrayphp.php

---
+++ arrayphp.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ *Data file for the project
+ */
+
+$arrBootstrap = array
+    (
+    'container' => '<div class="container"><!--CONTAINER ####################################################################################-->',
+    'row' => '<div class="row"><!-- ROW  #################################################################### -->',
+    'md12' => '<div class="col-md-12" id="row12"><p>col md 12</p></div>',
+    'md8' => '<div class="col-md-8"><p>col md 8</p></div>',
+    'md6' => '<div class="col-md-6"><p>col md 6</p></div>',
+    'md4' => '<div class="col-md-4"><p>col md 4</p></div>',
+    'md3' => '<div class="col-md-3"><p>col md 3</p></div>',
+    'md2' => '<div class="col-md-2"><p>col md 2</p></div>',
+    'md1' => '<div class="col-md-1"><p>col md 1</p></div>',
+    'closeDiv' => '</div><!--CLOSE DIV ####################################-->',
+    'md5' => '<div class="col-md-5"><p>col md 5</p></div>',
+    'md7' => '<div class="col-md-7"><p>col md 7</p></div>',
+    'md9' => '<div class="col-md-9"><p>col md 9</p></div>',
+    'md10' => '<div class="col-md-10"><p>col md 10</p></div>',
+    'md11' => '<div class="col-md-11"><p>col md 11</p></div>',
+    'styling' => '<style>   .col-md-11,.col-md-10,.col-md-9, .col-md-8, .col-md-7, .col-md-6, .col-md-5, .col-md-4, .col-md-3, .col-md-2, .col-md-1, #row12  { border-style: dotted;} </style>',
+        
+    );
+
+<?php include "includedFiles/codeBlockArray.php"; print $arrBootstrap['styling'];  ?>
